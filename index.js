c0abd222a005d662b063b350913c9b84584bba9a
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-25 11:13:31 +0600

    start ema john server

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6soco.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        // connect database
+        await client.connect;
+        console.log('connection success');
+
+        // create database and collection
+        const database = client.db('emaJohn_Shop');
+        const productsCollection = database.collection('products');
+
+        app.get('/products', async (req, res) => {
+            const cursor = productsCollection.find({});
+            const products = await cursor.toArray();
+            res.send(products);
+        })
+
+
+    }
+    finally {
+        //    await client.close;
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('ema john server running');
+})
+
+app.listen(port, () => {
+    console.log(`ema-john-simple listening at http://localhost:${port}`)
+})
